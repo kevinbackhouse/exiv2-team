bad806ca06e46bd4a694054d4ae5e1f52fedae4e
Josh Soref <jsoref@users.noreply.github.com>
2022-02-05 22:54:09 -0500

    spelling: hung up
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- rmills/bin/farmer.cpp
+++ rmills/bin/farmer.cpp
@@ -211,7 +211,7 @@ int forgive(int n,int& err)
 	err  = WSAGetLastError() ;
 	if ( !n && !err ) return FINISH ;
 #ifndef WIN32
-	if ( n == 0 ) return FINISH ; // server hungup
+	if ( n == 0 ) return FINISH ; // server hung up
 #endif
 	bool bForgive = err == WSAEWOULDBLOCK || err == WSAENOTCONN ;
 	bool bError   = n == SOCKET_ERROR ;
